8705280ef4b43b88cc0f5ec672d206a1e25346ff
김율희 <dbfgml741@likelion.org>
2020-11-12 22:02:48 +0900

    delete.jade

--- app_mysql.js
+++ app_mysql.js
@@ -15,7 +15,7 @@ var mysql = require('mysql');
 var conn = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '!khc532412',
+  password : '',
   database : 'o2'
 });
 conn.connect();
