16a49f67b0626a2198accb4b001cd65e22b726a2
김율희 <dbfgml741@likelion.org>
2020-12-01 22:02:48 +0900

    list

--- app_mysql.js
+++ app_mysql.js
@@ -15,7 +15,7 @@ var mysql = require('mysql');
 var conn = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '111111',
+  password : '',
   database : 'o2'
 });
 conn.connect();
