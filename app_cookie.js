894aa903b74bfe3d54d820d1d0add24e09ccc6ae
김율희 <dbfgml741@likelion.org>
2021-01-12 22:02:48 +0900

    app_cookie.js(1)

--- app_cookie.js
+++ app_cookie.js
@@ -2,6 +2,22 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var app = express();
 app.use(cookieParser());
+
+var products = {
+    1:{title: 'The history of web 1'},
+    2:{title: 'The next web'}
+};
+app.get('/products', function(req, res){
+    var output = '';
+    for(var i in products){
+        output += `
+        <li>
+            <a href="/cart/${i}">${products[i].title}</a>
+        </li>`
+    }
+    res.send(`<h1>Products</h1><ul>${output}</ul><a href="/cart">Cart</a>`);
+});
+
 app.get('/count', function(req, res){
     if(req.cookies.count){
         var count = parseInt(req.cookies.count);
